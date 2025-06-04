a8365a3426147c6952c2e967a1effd4ffe1c4c91
AbdulsemedM <abdumussema22@gmail.com>
2025-06-04 14:21:22 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/twf/pos/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.hudhud.pos
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/helpers/style.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class StyleColors {
+  mainColor(double opacity) {
+    return Color(0xFFFF5A00);
+    // return Colors.blue.withOpacity(opacity);
+  }
+
+  secondColor(double opacity) {
+    return Color(0xFF5b3895);
+  }
+
+  accentColor(double opacity) {
+    return Color(0xFF5b3895);
+  }
+}
